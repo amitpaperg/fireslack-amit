61c7b3023cc92f3b02ab27e4b1c2067a682ccdf3
Amit Velingkar <amit.velingkar@paperg.com>
2016-02-14 15:43:06 -0800

    Basic Auth + User Profiles Service

--- app/app.js
+++ app/app.js
@@ -1,34 +1,76 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc overview
- * @name angularfireSlackApp
- * @description
- * # angularfireSlackApp
- *
- * Main module of the application.
- */
-angular
-  .module('angularfireSlackApp', [
-    'firebase',
-    'angular-md5',
-    'ui.router'
-  ])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'home/home.html'
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'auth/login.html'
-      })
-      .state('register', {
-        url: '/register',
-        templateUrl: 'auth/register.html'
-      });
+  /**
+   * @ngdoc overview
+   * @name angularfireSlackApp
+   * @description
+   * # angularfireSlackApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('angularfireSlackApp', [
+      'firebase',
+      'angular-md5',
+      'ui.router'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'home/home.html'
+        })
+        .state('login', {
+          url: '/login',
+          controller: 'AuthController',
+          controllerAs: 'vm',
+          templateUrl: 'auth/login.html',
+          resolve: {
+            requireNoAuth: function($state, Auth) {
+              return Auth.$requireAuth().then(function(auth){
+                $state.go('home');
+              }, function(error) {
+                return;
+              });
+            }
+          }
+        })
+        .state('register', {
+          url: '/register',
+          controller: 'AuthController',
+          controllerAs: 'vm',
+          templateUrl: 'auth/register.html',
+          resolve: {
+            requireNoAuth: function($state, Auth) {
+              return Auth.$requireAuth().then(function(auth){
+                $state.go('home');
+              }, function(error) {
+                return;
+              });
+            }
+          }
+        })
+        .state('profile', {
+          url: '/profile',
+          controller: 'ProfileController',
+          controllerAs: 'vm',
+          templateUrl: 'users/profile.html',
+          resolve: {
+            auth: function($state, Users, Auth) {
+              return Auth.$requireAuth().catch(function(){
+                $state.go('home');
+              });
+            },
+            profile: function(Users, Auth) {
+              return Auth.$requireAuth().then(function(auth){
+                return Users.getProfile(auth.uid).$loaded();
+              });
+            }
+          }
+        });
 
-    $urlRouterProvider.otherwise('/');
-  })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+      $urlRouterProvider.otherwise('/');
+    })
+    .constant('FirebaseUrl', 'https://fireslack-amit.firebaseio.com/');
+})();
